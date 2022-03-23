11441e87ae0e93b24af818c14242ba7c5544115e
Markus Mueller <35893808+muellermarkus@users.noreply.github.com>
2022-03-23 15:44:48 +0100

    embedding script

---
+++ scripts/word_embeddings.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import spacy
+
+# download language model
+# !python -m spacy download en_core_web_lg
+# nlp = spacy.load('en_core_web_lg', disable=["tagger", "attribute_ruler", "lemmatizer"])
+
+nlp = spacy.load('en_core_web_lg')
+
+
+# remove stop words ?
+
+# load the meta data
+data_path = Path(__file__).parent.parent / 'data'
+df = pd.read_parquet(data_path / 'cleaned_data.gzip')
+
+# process titles, get mean vector for each title (300 dimensional)
+raw_titles = df['clean_title'].to_list()
+title_embeds = [nlp(raw_title).vector for raw_title in raw_titles]
+df['title_embed'] = title_embeds
+del title_embeds, raw_titles
+
+
+
+# process descriptions, get mean vector for each description (300 dimensional)
+raw_descs = df['clean_description'].to_list()
+desc_embeds = [nlp(raw_desc).vector for raw_desc in raw_descs]
+df['description_embed'] = desc_embeds
+del desc_embeds, raw_descs
+
+
+
+
+df.drop(labels = ['cleaned_title, cleaned_description'], axis = 1, inplace = True)
+df.to_parquet(data_path / 'cleaned_data.gzip', compression = 'gzip')
+
+
+
+
+# check if vectors of books in a small subcategory are similar
+
+
+
