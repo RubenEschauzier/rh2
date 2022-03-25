a90476f4ec84cf35e1bac7e11faffc89bb858ed3
muellermarkus <markus.mueller939@gmail.com>
2022-03-25 07:54:18 +0100

    create word embeddings

--- scripts/word_embeddings.py
+++ scripts/word_embeddings.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 import spacy
-
+from tqdm import tqdm
 # download language model
 # !python -m spacy download en_core_web_lg
 # nlp = spacy.load('en_core_web_lg', disable=["tagger", "attribute_ruler", "lemmatizer"])
@@ -18,7 +18,7 @@ df = pd.read_parquet(data_path / 'cleaned_data.gzip')
 
 # process titles, get mean vector for each title (300 dimensional)
 raw_titles = df['clean_title'].to_list()
-title_embeds = [nlp(raw_title).vector for raw_title in raw_titles]
+title_embeds = [nlp(raw_title).vector for raw_title in tqdm(raw_titles)]
 df['title_embed'] = title_embeds
 del title_embeds, raw_titles
 
@@ -26,15 +26,15 @@ del title_embeds, raw_titles
 
 # process descriptions, get mean vector for each description (300 dimensional)
 raw_descs = df['clean_description'].to_list()
-desc_embeds = [nlp(raw_desc).vector for raw_desc in raw_descs]
+desc_embeds = [nlp(raw_desc).vector for raw_desc in tqdm(raw_descs)]
 df['description_embed'] = desc_embeds
 del desc_embeds, raw_descs
 
 
 
 
-df.drop(labels = ['cleaned_title, cleaned_description'], axis = 1, inplace = True)
-df.to_parquet(data_path / 'cleaned_data.gzip', compression = 'gzip')
+df.drop(labels = ['clean_title', 'clean_description'], axis = 1, inplace = True)
+df.to_parquet(data_path / 'embed_data.gzip', compression = 'gzip')
 
 
 
